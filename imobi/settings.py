b2578e4b8cda7d95153c7b570314933b857de768
Michele J <90045067+Jasmim-mii@users.noreply.github.com>
2025-01-27 12:38:05 -0300

    Update settings.py

--- imobi/settings.py
+++ imobi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b+lr4y0q(r4wmz!a)ev503+5!sjy*j30lj=^m3))9o%#yme@g(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.build_files.sh']
 
 
 # Application definition
